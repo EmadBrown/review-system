1f817f60b1bf49f488756f48609815071acb9508
EmadBrown <emadbrown2013@gmail.com>
2017-12-18 11:59:39 +0100

    Fix view contact email

--- resources/views/email/contact.blade.php
+++ resources/views/email/contact.blade.php
@@ -33,7 +33,9 @@
                                                     <tbody>
                                                         <tr>
                                                             <td style="font-family: Avenir, Helvetica, sans-serif; box-sizing: border-box;">
+                                                                <form method="POST" action="route('review.delete', [$id])" accept-charset="UTF-8">
                                                                 <a target="_blank" href="#" class="button button-blue" style="font-family: Avenir, Helvetica, sans-serif; box-sizing: border-box; border-radius: 3px; box-shadow: 0 2px 3px rgba(0, 0, 0, 0.16); color: #FFF; display: inline-block; text-decoration: none; -webkit-text-size-adjust: none; background-color: #3097D1; border-top: 10px solid #3097D1; border-right: 18px solid #3097D1; border-bottom: 10px solid #3097D1; border-left: 18px solid #3097D1;">Save Review</a>
+                                                                </form>
                                                             </td>
                                                               <td style="font-family: Avenir, Helvetica, sans-serif; box-sizing: border-box;">
                                                                   
