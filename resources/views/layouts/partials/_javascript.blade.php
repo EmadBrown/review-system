72c59162cf05245a14796e10adbcd82007b12132
EmadBrown <emadbrown2013@gmail.com>
2017-12-22 11:04:58 +0100

    Uncomment Jquery library in bootstap.js

--- resources/views/layouts/partials/_javascript.blade.php
+++ resources/views/layouts/partials/_javascript.blade.php
@@ -1,5 +1,3 @@
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}"></script>
     
-     <!-- JQuery -->
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
